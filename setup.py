bf416be3e479adc83133fb2d69ad9ad31d65cc16
Oddball74 <mica.assistant@proton.me>
2026-01-12 22:02:14 +0100

    much better setup.py

--- setup.py
+++ setup.py
@@ -5,24 +5,36 @@ from tools import set_script_as_global, shortcutCreator
 
 def setup():
 
-    print("Making scripts available everywhere")
-    os.chdir(os.path.dirname(__file__))
-    set_script_as_global.set("mica")
+    try:
+        print("Making scripts available everywhere")
+        os.chdir(os.path.dirname(__file__))
+        set_script_as_global.set("mica")
+    except:
+        print("Whoops, something happened")
 
-    print("Creating shortcut")
-    os.chdir(os.path.join(os.path.dirname(__file__),"tools"))
-    print(os.getcwd())
-    target = os.path.join(os.path.dirname(__file__),"mica.py")
-    print(target)
-    shortcutCreator.create_shortcut(name="Mica Assistant",target=target,icon=os.path.join(os.path.dirname(__file__),"tools","mica.ico"))
-    filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)),"docs","index.html")
-    #opening the presentation page
-    if platform.system() == 'Darwin':       # macOS
-        subprocess.call(('open', filepath))
-    elif platform.system() == 'Windows':    # Windows
-        os.startfile(filepath)
-    else:                                   # linux variants
-        subprocess.call(('xdg-open', filepath))
+    try:
+        print("Creating shortcut")
+        os.chdir(os.path.join(os.path.dirname(__file__),"tools"))
+        print(os.getcwd())
+        target = os.path.join(os.path.dirname(__file__),"mica.py")
+        print(target)
+        shortcutCreator.create_shortcut(name="Mica Assistant",target=target,icon=os.path.join(os.path.dirname(__file__),"tools","mica.ico"))
+        filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)),"docs","index.html")
+    except:
+        print("Whoops, something happened")
+
+    os.system('pip install prompt_toolkit')#its the only one we have to install first
+
+    try:
+        #opening the presentation page
+        if platform.system() == 'Darwin':       # macOS
+            subprocess.call(('open', filepath))
+        elif platform.system() == 'Windows':    # Windows
+            os.startfile(filepath)
+        else:                                   # linux variants
+            subprocess.call(('xdg-open', filepath))
+    except:
+        print("Whoops, something happened")
     print("Setup complete, will terminate in 5 seconds")
     time.sleep(5)
 if __name__ == "__main__":
